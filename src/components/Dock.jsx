f499b15f4395886bd74f56f569ed761277327b59
Abdu Alim Arlikhozhaev <arlikhozhaevca@gmail.com>
2026-01-27 19:10:54 -0800

    Implement window management HOC and terminal window

--- src/components/Dock.jsx
+++ src/components/Dock.jsx
@@ -1,13 +1,14 @@
 import { useRef } from "react"
-import { Tooltip } from "react-tooltip"
+
 import gsap from "gsap"
+import { useGSAP } from "@gsap/react"
+import { Tooltip } from "react-tooltip"
 
 import { dockApps } from "#constants"
-import { useGSAP } from "@gsap/react"
-
-
+import useWindowStore from "#store/window"
 
 const Dock = () => {
+    const { openWindow, closeWindow, windows } = useWindowStore();
     const dockRef = useRef(null)
 
     useGSAP(() => {
@@ -61,7 +62,20 @@ const Dock = () => {
     }, []);
 
     const toggleApp = (app) => {
-        // TODO Implement Open Window Logic
+        if(!app.canOpen) return;
+
+        const window = windows[app.id];
+
+        if(!window) {
+            console.error(`Window not found for app: ${app.id}`);
+            return;
+        }
+
+        if(window.isOpen) {
+            closeWindow(app.id);
+        } else {
+            openWindow(app.id);
+        }
     }
 
     return (
